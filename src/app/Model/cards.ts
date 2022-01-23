12763c14e99a73024144f5bd37f05ac294364dab
Abdullah Aladham <abdullah.aladham@gmail.com>
2022-01-24 01:32:29 +0200

    authguard and minor changes

---
+++ src/app/Model/cards.ts
@@ -0,0 +1,7 @@
+export interface cards{
+    id:number;
+    name:string;
+    cardcode:string;
+    imagesrc:string;
+    imagesrc2:string;
+}
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -9,10 +9,18 @@ export class AuthGuard implements CanLoad {
   constructor(private router:Router){}
   canLoad() {
     const isAuthenticated=!!(+localStorage.getItem('authenticated'));
+    // const isAuthenticated=localStorage.getItem('authenticated')
 
     if(isAuthenticated){
       return true;
     } else{
+      const navigation =this.router.getCurrentNavigation();
+      console.log('nav:',navigation);
+      let url='/';
+      if(navigation){
+        url=navigation.extractedUrl.toString();
+      }
+      this.router.navigateByUrl('/login')
       return false;
     }
   }
